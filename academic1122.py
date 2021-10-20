6e7e14e01a2aceef342eb72a36c2efd85a38ee4e
Ariel Pellegrino <ariel.pell@gmail.com>
2021-10-19 22:55:21 -0300

    subindo arquivos URI

---
+++ academic1122.py
@@ -0,0 +1,10 @@
+
+
+A = int(input()) #Percurso da casa até primeiro emprego
+B = int(input()) #Percurso do primeiro emprego ao segunfo emprego
+C = int(input()) #Percurso do segundo emprego até o retorno para casa
+
+tempoGasto = A + B + C
+
+
+print(tempoGasto,'minutos')
---
+++ academic1123.py
@@ -0,0 +1,5 @@
+
+polegadas = float(2.54)
+calculo = float(input())
+
+print('{:.3f}'.format(calculo * polegadas))
